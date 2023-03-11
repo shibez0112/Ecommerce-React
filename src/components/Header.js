b50a34d2d83ac2caef416b569555819a06649ea1
shibez <ginta2888@gmail.com>
2023-03-11 21:25:33 +0700

    feat: Add new icon for header bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,17 +35,53 @@ const Header = () => {
               <div className="input-group mb-3">
                 <input
                   type="text"
-                  className="form-control"
+                  className="form-control py-2"
                   placeholder="Search Product Here..."
                   aria-label="Search Product Here..."
                   aria-describedby="basic-addon2"
                 />
-                <span className="input-group-text" id="basic-addon2">
-                  <BsSearch/>
+                <span className="input-group-text p-3" id="basic-addon2">
+                  <BsSearch className="fs-6" />
                 </span>
               </div>
             </div>
-            <div className="col-5"></div>
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/compare.svg" alt="compare" />
+                    <p className="mb-0">
+                      Compare <br /> Products
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/wishlist.svg" alt="wishlist" />
+                    <p className="mb-0">
+                      Favourite <br /> wishlist
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/user.svg" alt="user" />
+                    <p className="mb-0">
+                      Log in <br /> My Account
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/cart.svg" alt="cart" />
+                    <div className="d-flex flex-column gap-10">
+                      <span className="badge bg-white text-dark">0</span>
+                      <p className="mb-0">$ 500</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </header>
