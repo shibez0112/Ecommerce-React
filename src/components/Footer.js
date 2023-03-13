31465525397458f8b2343431378e5c2ff0ae5aa9
shibez <ginta2888@gmail.com>
2023-03-13 11:51:19 +0700

    feat: Add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,124 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { BsLinkedin, BsGithub, BsYoutube, BsInstagram } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-5">
+              <div className="footer-top-data d-flex gap-30 align-items-center">
+                <img
+                  className="img-filter"
+                  src="images/newsletter.svg"
+                  alt=""
+                  height={30}
+                  width={30}
+                />
+                <h2 className="mb-0 text-white">Sign Up for Newsletter</h2>
+              </div>
+            </div>
+            <div className="col-7">
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control py-1"
+                  placeholder="Your Email"
+                  aria-label="Your Email"
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-2" id="basic-addon2">
+                  Subscribe
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-4">
+              <h4 className="text-white mb-4">Contact Us</h4>
+              <div>
+                <address className="text-white fs-6">
+                  Hno : Thu Duc Ward, <br /> Ho Chi Minh City <br />
+                  Pincode: 88888
+                </address>
+                <a
+                  href="tel: +84 3779273"
+                  className="mt-4 d-block mb-0 text-white"
+                >
+                  +84 3779273
+                </a>
+                <a
+                  href="mailto:pqtoanhcmus@gmail.com"
+                  className="mt-2 d-block mb-0 text-white"
+                >
+                  pqtoanhcmus@gmail.com
+                </a>
+                <div className="social-icons d-flex align-items-center gap-30 mt-4">
+                  <a className="text-white" href="">
+                    <BsLinkedin fs-4 />
+                  </a>
+                  <a className="text-white" href="">
+                    <BsInstagram fs-4 />
+                  </a>
+                  <a className="text-white" href="">
+                    <BsGithub fs-4 />
+                  </a>
+                  <a className="text-white" href="">
+                    <BsYoutube fs-4 />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Information</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Privacy Policy</Link>
+                <Link className="text-white py-2 mb-1">Refund Policy</Link>
+                <Link className="text-white py-2 mb-1">Shipping Policy</Link>
+                <Link className="text-white py-2 mb-1">Terms & Conditions</Link>
+                <Link className="text-white py-2 mb-1">Blogs</Link>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Account</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Aviyt Us</Link>
+                <Link className="text-white py-2 mb-1">Faq</Link>
+                <Link className="text-white py-2 mb-1">Contact</Link>
+                <Link className="text-white py-2 mb-1">Size Chart</Link>
+              </div>
+            </div>
+            <div className="col-2">
+              <h4 className="text-white mb-4">Quick Links</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Laptops</Link>
+                <Link className="text-white py-2 mb-1">Headphones</Link>
+                <Link className="text-white py-2 mb-1">Tablet</Link>
+                <Link className="text-white py-2 mb-1">Watch</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <p className="text-center mb-0 text-white">
+                &copy; {new Date().getFullYear()}; Powered by Shibez
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
