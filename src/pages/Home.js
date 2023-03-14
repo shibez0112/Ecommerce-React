c48bd1423d71e36c8f63c567052cb89c4b827710
shibez <ginta2888@gmail.com>
2023-03-14 09:12:07 +0700

    feat: Add 2 banner for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,203 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <section className="home-wrapper-1 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <div className="main-banner position-relative p-3">
+                <img
+                  src="images/main-banner-1.jpg"
+                  className="img-fluid rounded-3"
+                  alt="main-banner"
+                />
+                <div className="main-banner-content position-absolute">
+                  <h4>SUPER CAMERA FOR PROS</h4>
+                  <h5>iPhone 14</h5>
+                  <p>FROM $1999.00 or $51.00/mo</p>
+                  <Link className="button">BUY NOW</Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-01.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main-banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>Best Seller</h4>
+                    <h5>iPhone 14</h5>
+                    <p>
+                      FROM $1999.00 <br /> or $51.00/mo
+                    </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-02.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main-banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>New Arrival</h4>
+                    <h5>iPhone 14</h5>
+                    <p>
+                      FROM $1999.00 <br /> or $51.00/mo
+                    </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-03.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main-banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>Best Seller</h4>
+                    <h5>iPhone 14</h5>
+                    <p>
+                      FROM $1999.00 <br /> or $51.00/mo
+                    </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-04.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main-banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>Best Seller</h4>
+                    <h5>iPhone 14</h5>
+                    <p>
+                      FROM $1999.00 <br /> or $51.00/mo
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="services d-flex align-items-center justify-content-between">
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service.png" alt="services" />
+                  <div>
+                    <h6>Free Shipping</h6>
+                    <p>From all orders over $100</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service-02.png" alt="services" />
+                  <div>
+                    <h6>Daily Surprise Offers</h6>
+                    <p>Save up to 25% off</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service-03.png" alt="services" />
+                  <div>
+                    <h6>Support 24/7</h6>
+                    <p>Shop with an expert</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service-04.png" alt="services" />
+                  <div>
+                    <h6>Affordable Prices</h6>
+                    <p>Get Factory direct price</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service-05.png" alt="services" />
+                  <div>
+                    <h6>Secure Payments</h6>
+                    <p>100% Protected Payments</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="categories d-flex flex-wrap justify-content-between align-items-center">
+                <div className="d-flex gap-30 align-items-center">
+                  <div>
+                    <h6>Music & Gaming</h6>
+                    <p>10 Items</p>
+                  </div>
+                  <img src="images/camera.jpg" alt="" />
+                </div>
+                <div className="d-flex gap-30 align-items-center">
+                  <div>
+                    <h6>Camera</h6>
+                    <p>10 Items</p>
+                  </div>
+                  <img src="images/camera.jpg" alt="" />
+                </div>
+                <div className="d-flex gap-30 align-items-center">
+                  <div>
+                    <h6>Smart Tv</h6>
+                    <p>10 Items</p>
+                  </div>
+                  <img src="images/tv.jpg" alt="" />
+                </div>
+                <div className="d-flex gap-30 align-items-center">
+                  <div>
+                    <h6>Headphones</h6>
+                    <p>10 Items</p>
+                  </div>
+                  <img src="images/headphone.jpg" alt="" />
+                </div>
+                <div className="d-flex gap-30 align-items-center">
+                  <div>
+                    <h6>Music & Gaming</h6>
+                    <p>10 Items</p>
+                  </div>
+                  <img src="images/camera.jpg" alt="" />
+                </div>
+                <div className="d-flex gap-30 align-items-center">
+                  <div>
+                    <h6>Camera</h6>
+                    <p>10 Items</p>
+                  </div>
+                  <img src="images/camera.jpg" alt="" />
+                </div>
+                <div className="d-flex gap-30 align-items-center">
+                  <div>
+                    <h6>Smart Tv</h6>
+                    <p>10 Items</p>
+                  </div>
+                  <img src="images/tv.jpg" alt="" />
+                </div>
+                <div className="d-flex gap-30 align-items-center">
+                  <div>
+                    <h6>Headphones</h6>
+                    <p>10 Items</p>
+                  </div>
+                  <img src="images/headphone.jpg" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Home
+export default Home;
