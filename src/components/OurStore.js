5550fb2b59887aaed7de59120c8d17ec03d0bda5
shibez <ginta2888@gmail.com>
2023-03-15 16:10:41 +0700

    feat: Add React Helmet to Webstite to edit title

---
+++ src/components/OurStore.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BreadCrumb from "./BreadCrumb";
+import Meta from "./Meta";
+
+const OurStore = () => {
+  return (
+    <>
+      <Meta title={"OurStore"} />
+      <BreadCrumb title="Our Store" />
+    </>
+  );
+};
+
+export default OurStore;
