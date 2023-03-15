07ba15831a452f5d13e6bd6b5abafdca2db8b424
shibez <ginta2888@gmail.com>
2023-03-15 21:15:09 +0700

    feat: Add some filter for products

--- src/components/OurStore.js
+++ src/components/OurStore.js
@@ -7,6 +7,59 @@ const OurStore = () => {
     <>
       <Meta title={"OurStore"} />
       <BreadCrumb title="Our Store" />
+      <div className="store-wrapper home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Shop by Categories</h3>
+                <ul className="ps-0">
+                  <li>Watch</li>
+                  <li>Tv</li>
+                  <li>Camera</li>
+                  <li>Laptop</li>
+                </ul>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Filter By</h3>
+                <div>
+                  <h5 className="sub-title">Availabity</h5>
+                  <div class="form-check">
+                    <input
+                      class="form-check-input"
+                      type="checkbox"
+                      value=""
+                      id=""
+                    />
+                    <label class="form-check-label" for="">
+                      In Stock (1)
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <input
+                      class="form-check-input"
+                      type="checkbox"
+                      value=""
+                      id=""
+                      checked
+                    />
+                    <label class="form-check-label" for="">
+                      Out of Stock(0)
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Product Tags</h3>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Random Product</h3>
+              </div>
+            </div>
+            <div className="col-9"></div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
