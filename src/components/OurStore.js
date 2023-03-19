ff6dddc8a0b3bee1143ea3118fb2fa853e6c187e
shibez <ginta2888@gmail.com>
2023-03-19 17:01:38 +0700

    feat: Add Sort by bar

--- src/components/OurStore.js
+++ src/components/OurStore.js
@@ -2,7 +2,7 @@ import React from "react";
 import BreadCrumb from "./BreadCrumb";
 import Meta from "./Meta";
 import ReactStars from "react-rating-stars-component";
- 
+
 const OurStore = () => {
   return (
     <>
@@ -183,7 +183,37 @@ const OurStore = () => {
                 </div>
               </div>
             </div>
-            <div className="col-9"></div>
+            <div className="col-9">
+              <div className="filter-sort-grid">
+                <div className="d-flex align-items-center gap-10">
+                  <p className="mb-0">Sort By:</p>
+                  <select name="" id="" className="form-control form-select">
+                    <option value="manual">Featured</option>
+                    <option value="best-selling" selected="selected">
+                      Best selling
+                    </option>
+                    <option value="title-ascending" selected="selected">
+                      Alphabetically, A-Z
+                    </option>
+                    <option value="title-descending" selected="selected">
+                      Alphabetically, Z-A
+                    </option>
+                    <option value="price-ascending" selected="selected">
+                      Price, low to high
+                    </option>
+                    <option value="price-descending" selected="selected">
+                      Price, high to low
+                    </option>
+                    <option value="created-ascending" selected="selected">
+                      Date, old to new
+                    </option>
+                    <option value="created-descending" selected="selected">
+                      Date, new to old
+                    </option>
+                  </select>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
